6b0e4ac220a6fa3038667046c96b3efb5cc6920e
Dheemanth Aiyappa <78716782+Dheemanth-Aiyappa@users.noreply.github.com>
2021-04-09 18:47:35 +0530

    Update LoginPage.java

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,7 +4,4 @@ public class LoginPage {
 	public void loginPage() {
 		 System.out.println("LoginPage method");
 	 }
-	public void loginErrorPage() {
-		 System.out.println("login Error Page method");
-	 }
 }
